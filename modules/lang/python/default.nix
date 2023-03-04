b28f649e69b53a6750bd7a85fc2677f7a7a88922
klchen0112 <klchen0112@gmail.com>
2023-03-05 00:13:09 +0800

    add tensorflow packages

--- modules/lang/python/default.nix
+++ modules/lang/python/default.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   home.packages = with pkgs; [
-    (python3.withPackages (ps: with ps; [isort matplotlib numpy pandas torch torchvision]))
+    (python3.withPackages (ps: with ps; [isort matplotlib numpy pandas tensorflow torch torchvision]))
     # python3Full
     nodejs
     nodePackages.pyright
