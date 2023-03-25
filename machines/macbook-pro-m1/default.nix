23098c312102de49fbc13120bc1aee58fe578ad0
klchen0112 <klchen0112@gmail.com>
2023-03-25 20:49:40 +0800

    add skhd and yabai config

--- machines/macbook-pro-m1/default.nix
+++ machines/macbook-pro-m1/default.nix
@@ -9,12 +9,13 @@
 {
   config,
   pkgs,
+  user,
   ...
 }: {
   users.users.chenkailong = {
-      # macOS user
-      home = "/Users/chenkailong";
-      shell = pkgs.fish; # Default shell
+    # macOS user
+    home = "/Users/${user}";
+    shell = pkgs.fish; # Default shell
   };
 
   fonts = {
@@ -78,93 +79,165 @@
       enable = true;
       package = pkgs.emacsGit;
     };
+
     yabai = {
       enable = true;
       config = {
         external_bar = "all:0:26";
       };
-      extraConfig = ''
-        yabai -m config debug_output                  on
-        yabai -m config mouse_follows_focus           off
-        yabai -m config focus_follows_mouse           off
-        yabai -m config window_placement              second_child
-        yabai -m config window_topmost                off
-        yabai -m config window_opacity                on
-        yabai -m config window_opacity_duration       0.0
-        yabai -m config window_shadow                 on
-        yabai -m config window_border                 off
-        yabai -m config active_window_opacity         1.0
-        yabai -m config normal_window_opacity         0.85
-        yabai -m config split_ratio                   0.62
-        yabai -m config auto_balance                  off
-        yabai -m config layout                        bsp
-        yabai -m config top_padding                   0
-        yabai -m config bottom_padding                0
-        yabai -m config left_padding                  0
-        yabai -m config right_padding                 0
-        yabai -m config window_gap                    0
-        yabai -m rule --add app="emacs" title!="^$"   manage="on"
+      extraConfig = let
+        gaps = {
+          top = "4";
+          bottom = "24";
+          left = "4";
+          right = "4";
+          inner = "4";
+        };
+        color = {
+          focused = "0xE0808080";
+          normal = "0x00010101";
+          preselect = "0xE02d74da";
+        };
+      in ''
+        #!/usr/bin/env bash
+
+        # Uncomment to refresh ubersicht widget on workspace change
+        # Make sure to replace WIDGET NAME for the name of the ubersicht widget
+        #ubersicht_spaces_refresh_command="osascript -e 'tell application id \"tracesOf.Uebersicht\" to refresh widget id \"WIDGET NAME\"'"
+
+        # ===== Loading Scripting Additions ============
+
+        # See: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#macos-big-sur---automatically-load-scripting-addition-on-startup
+        sudo yabai --load-sa
+        yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+        # ===== Tiling setting =========================
+        yabai -m config layout                      bsp
+
+        yabai -m config top_padding                 ${gaps.top}
+        yabai -m config bottom_padding              ${gaps.bottom}
+        yabai -m config left_padding                ${gaps.left}
+        yabai -m config right_padding               ${gaps.right}
+        yabai -m config window_gap                  ${gaps.inner}
+
+        yabai -m config mouse_follows_focus         off
+        yabai -m config focus_follows_mouse         off
+
+        yabai -m config window_topmost              off
+        yabai -m config window_opacity              off
+        yabai -m config window_shadow               float
+
+        yabai -m config window_border               on
+        yabai -m config window_border_width         2
+        yabai -m config active_window_border_color  ${color.focused}
+        yabai -m config normal_window_border_color  ${color.normal}
+        yabai -m config insert_feedback_color       ${color.preselect}
+
+        yabai -m config active_window_opacity       1.0
+        yabai -m config normal_window_opacity       0.90
+        yabai -m config split_ratio                 0.50
+
+        yabai -m config auto_balance                off
+
+        yabai -m config mouse_modifier              fn
+        yabai -m config mouse_action1               move
+        yabai -m config mouse_action2               resize
+        # ===== Rules ==================================
+        yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
+        yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
+        yabai -m rule --add label="macfeh" app="^macfeh$" manage=off
+        yabai -m rule --add label="System Preferences" app="^System Preferences$" title=".*" manage=off
+        yabai -m rule --add label="App Store" app="^App Store$" manage=off
+        yabai -m rule --add label="Activity Monitor" app="^Activity Monitor$" manage=off
+        yabai -m rule --add label="KeePassXC" app="^KeePassXC$" manage=off
+        yabai -m rule --add label="Calculator" app="^Calculator$" manage=off
+        yabai -m rule --add label="Dictionary" app="^Dictionary$" manage=off
+        yabai -m rule --add label="mpv" app="^mpv$" manage=off
+        yabai -m rule --add label="Software Update" title="Software Update" manage=off
+        yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
         yabai -m rule --add app="Dash"                manage="off"
-        yabai -m rule --add app="System Preferences"  manage="off"
         yabai -m rule --add app="Plexamp"             manage="off"
+        # ===== Signals ================================
+
       '';
     };
     skhd = {
       enable = true;
-      skhdConfig = let
-        modMask = "cmd";
-        moveMask = "ctrl + cmd";
-        # myTerminal = "emacsclient -a '' -nc --eval '(peel/vterm)'";
-        myEditor = "emacsclient -a '' -nc";
-        myPlayer = "open /Applications/Plexamp.app";
-        noop = "/dev/null";
-        prefix = "yabai -m";
-        fstOrSnd = {
-          fst,
-          snd,
-        }: domain: "${prefix} ${domain} --focus ${fst} || ${prefix} ${domain} --focus ${snd}";
-        nextOrFirst = fstOrSnd {
-          fst = "next";
-          snd = "first";
-        };
-        prevOrLast = fstOrSnd {
-          fst = "prev";
-          snd = "last";
-        };
-        keycodes = import ./keycodes.nix;
-      in ''
-        # windows ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-        # select
-        ${modMask} - j                            : ${prefix} window --focus next || ${prefix} window --focus "$((${prefix} query --spaces --display next || ${prefix} query --spaces --display first) |${pkgs.jq}/bin/jq -re '.[] | select(.visible == 1)."first-window"')" || ${prefix} display --focus next || ${prefix} display --focus first
-        ${modMask} - k                            : ${prefix} window --focus prev || ${prefix} window --focus "$((yabai -m query --spaces --display prev || ${prefix} query --spaces --display last) | ${pkgs.jq}/bin/jq -re '.[] | select(.visible == 1)."last-window"')" || ${prefix} display --focus prev || ${prefix} display --focus last
-        # close
-        ${modMask} - ${keycodes.Delete}           : ${prefix} window --close && yabai -m window --focus prev
-        # fullscreen
-        ${modMask} - h                            : ${prefix} window --toggle zoom-fullscreen
-        # rotate
-        ${modMask} - r                            : ${prefix} window --focus smallest && yabai -m window --warp largest && yabai -m window --focus largest
-        # increase region
-        ${modMask} - ${keycodes.LeftBracket}      : ${prefix} window --resize left:-20:0
-        ${modMask} - ${keycodes.RightBracket}     : ${prefix} window --resize right:-20:0
-        # spaces ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-        # switch
-        ${modMask} + alt - j                      : ${prevOrLast "space"}
-        ${modMask} + alt - k                      : ${nextOrFirst "space"}
-        # send window
-        ${modMask} + ${moveMask} - j              : ${prefix} window --space prev
-        ${modMask} + ${moveMask} - k              : ${prefix} window --space next
-        # display  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-        # focus
-        ${modMask} - left                         : ${prevOrLast "display"}
-        ${modMask} - right                        : ${nextOrFirst "display"}
-        # send window
-        ${moveMask} - right                       : ${prefix} window --display prev
-        ${moveMask} - left                        : ${prefix} window --display next
-        # apps  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-        ${modMask} + shift - return               : ${myEditor}
-        ${modMask} - p                            : ${myPlayer}
-        # reset  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-        ${modMask} - q                            : pkill yabai; pkill skhd; osascript -e 'display notification "wm restarted"'
+      skhdConfig = ''
+        # opens WezTerm
+        alt - return : open \$\{HOME\}/Applications/Home Manager Apps/WezTerm.app
+
+
+        # Navigation
+        alt - h : yabai -m window --focus west
+        alt - j : yabai -m window --focus south
+        alt - k : yabai -m window --focus north
+        alt - l : yabai -m window --focus east
+
+        # Moving windows
+        shift + alt - h : yabai -m window --warp west
+        shift + alt - j : yabai -m window --warp south
+        shift + alt - k : yabai -m window --warp north
+        shift + alt - l : yabai -m window --warp east
+
+        # Move focus container to workspace
+        shift + alt - m : yabai -m window --space last; yabai -m space --focus last
+        shift + alt - p : yabai -m window --space prev; yabai -m space --focus prev
+        shift + alt - n : yabai -m window --space next; yabai -m space --focus next
+        shift + alt - 1 : yabai -m window --space 1; yabai -m space --focus 1
+        shift + alt - 2 : yabai -m window --space 2; yabai -m space --focus 2
+        shift + alt - 3 : yabai -m window --space 3; yabai -m space --focus 3
+        shift + alt - 4 : yabai -m window --space 4; yabai -m space --focus 4
+
+        # Move focus container to monitor
+        lctrl + alt - m : yabai -m window --display last; yabai -m display --focus last
+        lctrl + alt - p : yabai -m window --display prev; yabai -m display --focus prev
+        lctrl + alt - n : yabai -m window --display next; yabai -m display --focus next
+        lctrl + alt - 1 : yabai -m window --display 1; yabai -m display --focus 1
+        lctrl + alt - 2 : yabai -m window --display 2; yabai -m display --focus 2
+        lctrl + alt - 3 : yabai -m window --display 3; yabai -m display --focus 3
+        lctrl + alt - 4 : yabai -m window --display 4; yabai -m display --focus 4
+
+        # Resize windows
+        lctrl + alt - h : yabai -m window --resize left:-50:0; \
+                          yabai -m window --resize right:-50:0
+        lctrl + alt - j : yabai -m window --resize bottom:0:50; \
+                          yabai -m window --resize top:0:50
+        lctrl + alt - k : yabai -m window --resize top:0:-50; \
+                          yabai -m window --resize bottom:0:-50
+        lctrl + alt - l : yabai -m window --resize right:50:0; \
+                          yabai -m window --resize left:50:0
+
+        # Equalize size of windows
+        lctrl + alt - e : yabai -m space --balance
+
+        # Enable / Disable gaps in current workspace
+        lctrl + alt - g : yabai -m space --toggle padding; yabai -m space --toggle gap
+
+        # Rotate windows clockwise and anticlockwise
+        alt - r         : yabai -m space --rotate 270
+        shift + alt - r : yabai -m space --rotate 90
+
+        # Rotate on X and Y Axis
+        shift + alt - x : yabai -m space --mirror x-axis
+        shift + alt - y : yabai -m space --mirror y-axis
+
+        # Set insertion point for focused container
+        shift + lctrl + alt - h : yabai -m window --insert west
+        shift + lctrl + alt - j : yabai -m window --insert south
+        shift + lctrl + alt - k : yabai -m window --insert north
+        shift + lctrl + alt - l : yabai -m window --insert east
+
+        # Float / Unfloat window
+        shift + alt - space : \
+            yabai -m window --toggle float; \
+            yabai -m window --toggle border
+
+        # Restart Yabai and Skhd
+        shift + lctrl + alt - r : pkill yabai; pkill skhd; osascript -e 'display notification "wm restarted"'
+
+        # Make window native fullscreen
+        alt - f         : yabai -m window --toggle zoom-fullscreen
+        shift + alt - f : yabai -m window --toggle native-fullscreen
       '';
     };
     spacebar = {
@@ -272,6 +345,7 @@
       "raycast"
       "openvpn-connect"
       "zotero"
+      # "skim"
       # "arc"
       # "via"
       "logseq"
