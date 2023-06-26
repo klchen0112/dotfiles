53d9ec91a2dfa7f9e5bda7ef1d57c89dcb58df74
klchen0112 <klchen0112@gmail.com>
2023-06-26 19:07:13 +0800

    use home-manager texlive

--- modules/lang/latex/default.nix
+++ modules/lang/latex/default.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    texlive.combined.scheme-full
-  ];
+  programs.texlive = {
+    enable = true;
+    package = pkgs.texlive.combined.scheme-full;
+  };
+
 }
