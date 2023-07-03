6b2e25e17c274897de78e63e5faa66c0e44c5ad0
klchen0112 <klchen0112@gmail.com>
2023-07-03 16:47:11 +0800

    update darwin doc

--- flake.nix
+++ flake.nix
@@ -19,11 +19,19 @@
   inputs =
     # All flake references used to build my NixOS setup. These are dependencies.
     {
-      nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # Nix Packages
-      nixos-hardware.url = "github:NixOS/nixos-hardware";
+      nixpkgs.url = "github:nixos/nixpkgs/release-23.05"; # Nix Packages
 
-      flake-utils.url = "github:numtide/flake-utils";
-      flake-parts.url = "github:hercules-ci/flake-parts";
+      nixos-hardware = {
+        url = "github:NixOS/nixos-hardware";
+      };
+
+      flake-utils = {
+        url = "github:numtide/flake-utils";
+      };
+
+      flake-parts = {
+        url = "github:hercules-ci/flake-parts";
+      };
 
       home-manager = {
         # User Package Management
@@ -73,91 +81,90 @@
       # fonts
     };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    flake-utils,
-    darwin,
-    nur,
-    nixgl,
-    nixos-wsl,
-    emacs-overlay,
-    nix-doom-emacs,
-    hyprland,
-    vscode-server,
-    ...
-  }:
-  # Function that tells my flake which to use and what do what to do with the dependencies.
-  let
-    # Variables that can be used in the config files.
-    username = "chenkailong";
-    location = "$HOME/.config";
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # Accessible through 'nix develop' or 'nix-shell' (legacy)
-    legacyPackages = forAllSystems (system:
-      import inputs.nixpkgs {
-        inherit system;
-        # This adds our overlays to pkgs
-        overlays = [
-          emacs-overlay.overlay
-        ];
-        # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-        # Instead, you should set nixpkgs configs here
-        # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
-        config.allowUnfree = true;
-        config.allowBroken = true;
-        # config.allowUnsupportedSystem = true;
-      });
-  in rec {
-    nixosConfigurations = {
-      # NixOS configurations
-      "wsl" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs username;
-        };
-        modules = [
-          ./machines/wsl
-          vscode-server.nixosModule
-          ({
-            config,
-            pkgs,
-            ...
-          }: {
-            services.vscode-server.enable = true;
-          })
-          home-manager.darwinModules.home-manager
-          modules/hosts/wsl/default.nix
-        ];
-      };
-    };
-    darwinConfigurations =
-      let
-        system = "aarch64-darwin";
-      in
-      {
-        "macbook-pro-m1" = darwin.lib.darwinSystem {
+  outputs =
+    inputs @ { self
+    , nixpkgs
+    , home-manager
+    , flake-utils
+    , darwin
+    , nur
+    , nixgl
+    , nixos-wsl
+    , emacs-overlay
+    , nix-doom-emacs
+    , hyprland
+    , vscode-server
+    , ...
+    }:
+    # Function that tells my flake which to use and what do what to do with the dependencies.
+    let
+      # Variables that can be used in the config files.
+      username = "chenkailong";
+      location = "$HOME/.config";
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      # Accessible through 'nix develop' or 'nix-shell' (legacy)
+      legacyPackages = forAllSystems (system:
+        import inputs.nixpkgs {
           inherit system;
+          # This adds our overlays to pkgs
+          overlays = [
+            emacs-overlay.overlay
+          ];
+          # NOTE: Using `nixpkgs.config` in your NixOS config won't work
+          # Instead, you should set nixpkgs configs here
+          # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
+          config.allowUnfree = true;
+          config.allowBroken = true;
+          # config.allowUnsupportedSystem = true;
+        });
+    in
+    rec {
+      nixosConfigurations = {
+        # NixOS configurations
+        "wsl" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = {
-            inherit username inputs system;
+            inherit inputs username;
           };
-            pkgs = legacyPackages.aarch64-darwin;
-              modules = [
-                  {documentation.enable = false;}
-                    # Modules that are used
-                    ./machines/macbook-pro-m1
-                    home-manager.darwinModules.home-manager
-                    modules/hosts/macbook-pro-m1/default.nix
+          modules = [
+            ./machines/wsl
+            vscode-server.nixosModule
+            ({ config
+             , pkgs
+             , ...
+             }: {
+              services.vscode-server.enable = true;
+            })
+            home-manager.darwinModules.home-manager
+            modules/hosts/wsl/default.nix
           ];
         };
       };
+      darwinConfigurations =
+        let
+          system = "aarch64-darwin";
+        in
+        {
+          "macbook-pro-m1" = darwin.lib.darwinSystem {
+            inherit system;
+            specialArgs = {
+              inherit username inputs system;
+            };
+            pkgs = legacyPackages.aarch64-darwin;
+            modules = [
+              # Modules that are used
+              ./machines/macbook-pro-m1
+              home-manager.darwinModules.home-manager
+              modules/hosts/macbook-pro-m1/default.nix
+            ];
+          };
+        };
 
-  };
+    };
 }
