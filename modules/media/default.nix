8fb3f67d622b5af312a72dfc5c05ff455e85dc14
klchen0112 <klchen0112@gmail.com>
2023-08-07 16:20:37 +0800

    remove anki

--- modules/media/default.nix
+++ modules/media/default.nix
@@ -1,16 +1,25 @@
 #
 # ../../media
 #
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.mpv = {
     enable = true;
   };
   programs.sioyek = {
     enable = true;
     #  package = pkgs.sioyek;
+    bindings = [
+
+      "move_up" = "p";
+      "move_down" = "n";
+      "move_left" = "b";
+      "move_right" = "f";
+      "screen_down" = [ "d" "<C-d>" ];
+      "screen_up" = [ "u" "<C-u>" ];
+    ];
+
   };
   home.packages = with pkgs; [
-    anki
     # calibre
     raycast
   ];
