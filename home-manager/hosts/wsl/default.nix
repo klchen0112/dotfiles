2856e593dcda6d182b47cae3aad6c7001c6bdee0
klchen0112 <klchen0112@gmail.com>
2023-08-12 21:23:47 +0800

    modify homemanger

--- home-manager/hosts/wsl/default.nix
+++ home-manager/hosts/wsl/default.nix
@@ -1,29 +1,76 @@
-{pkgs, user,...}: {
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, username
+, ...
+}: {
   imports = [
-    #  ../../browser
-    ../../editors/emacs
+    inputs.nix-doom-emacs.hmModule
+
+    ../../browser
+    ../../downloader
+    # ../../editors/codeblocks
+    ../../editors/jetbrains
+    ../../editors/emacs/default.nix
+    # ../../editors/vim
     # ../../editors/nvim
-    # ../../editors/vscode
-    # ../../lang/cpp
-    # ../../lang/julia
-    # ../../lang/lua
+    ../../editors/vscode
+    ../../graphics
+    ../../keyboard
+    ../../lang/cpp
+    ../../lang/go
+    ../../lang/json
+    ../../lang/julia
+    ../../lang/lua
     ../../lang/latex
     ../../lang/markdown
     ../../lang/nix
-    # ../../lang/nodejs
-    # ../../lang/python
-    # ../../reader
-    ../../shells/git
-    ../../shells/fish
-    # ../../shells/zsh
-    ../../shells/shell
+    ../../lang/nodejs
+    ../../lang/python
+    ../../lang/rust
+    ../../media
+    ../../network
+    ../../notes
+    ../../office
+    ../../shells
+    ../../socialMedia
     ../../terminal
+    ../../vm
     # ../../vpn
   ];
 
-  home = {
-    username = "${user}";
-    homeDirectory = "/Users/${user}";
-    stateVersion = "23.05";
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
   };
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+  };
+  home.stateVersion = "23.05";
+
 }
