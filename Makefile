7fc082373291b9a13a75c5e52bd51ef21f8c85c0
klchen0112 <klchen0112@gmail.com>
2023-09-25 22:18:35 +0800

    add i12500 update

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 # nix
 
-up:
-	nix flake update
+i12500:
+	sudo nixos-rebuild switch --flake ~/myOpenSource/dotfiles#i12500
 
+klcheni12500:
+	home-manager switch --flake ~/myOpenSource/dotfiles#klchen@i12500
 
 darwin:
 	darwin-rebuild switch --flake .#macbook-pro-m1
 
+
+up:
+	nix flake update
 
 fmt:
 	nix fmt
--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       "https://cache.nixos.org"
       "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.cernet.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
