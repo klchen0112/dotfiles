1b293615d921399194a75c62a12db1801ae45f3f
klchen0112 <klchen0112@gmail.com>
2023-10-11 22:47:58 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -253,7 +253,7 @@
       };
       # fonts
       apple-fonts = {
-        url = "github:klchen0112/apple-fonts.nix";
+        url = "github:Lyndeno/apple-fonts.nix";
         inputs.flake-utils.follows = "flake-utils";
         inputs.nixpkgs.follows = "nixpkgs";
       };
