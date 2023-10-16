2720d186030ab71e96e93ddc68db7e99214d5a5d
klchen0112 <klchen0112@gmail.com>
2023-10-16 23:34:18 +0800

    add dvisvgm and dvipng

--- home-manager/lang/latex/default.nix
+++ home-manager/lang/latex/default.nix
@@ -2,7 +2,10 @@
   programs.texlive = {
     enable = true;
     extraPackages = tpkgs: {
-      inherit (tpkgs) scheme-small;
+      inherit (tpkgs)
+        scheme-small
+        dvisvgm dvipng # for preview and export as html
+        wrapfig amsmath ulem hyperref capt-of;
     };
 
   };
