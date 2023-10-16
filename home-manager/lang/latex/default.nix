25958eb14e1fb5af88f50558fbabfc420eedd677
klchen0112 <klchen0112@gmail.com>
2023-10-16 14:54:37 +0800

    add texlive

--- home-manager/lang/latex/default.nix
+++ home-manager/lang/latex/default.nix
@@ -1,22 +1,9 @@
 { pkgs, ... }: {
-  # programs.texlive = {
-  #   enable = true;
-  #   extraPackages = tpkgs: { inherit (tpkgs) collection-xetex; };
-  #   # packageSet = pkgs.texlive.combine {
-  #   #   inherit
-  #   #     (pkgs.texlive)
-  #   #     scheme-basic
-  #   #     dvisvgm
-  #   #     dvipng # for preview and export as html
-  #   #     wrapfig
-  #   #     amsmath
-  #   #     ulem
-  #   #     hyperref
-  #   #     capt-of
-  #   #     ;
-  #   #   #(setq org-latex-compiler "lualatex")
-  #   #   #(setq org-preview-latex-default-process 'dvisvgm)
-  #   # };
-  # };
-  home.packages = with pkgs; [ texlive.combined.scheme-full ];
+  programs.texlive = {
+   enable = true;
+   extraPackages = tpkgs: {
+      inherit (tpkgs) scheme-small;
+    };
+
+  };
 }
--- machines/macbook-pro-m1/default.nix
+++ machines/macbook-pro-m1/default.nix
@@ -25,7 +25,7 @@
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
-      inputs.nixpkgs-firefox-darwin.overlay
+      # inputs.nixpkgs-firefox-darwin.overlay
 
       # Or define it inline, for example:
       # (final: prev: {
