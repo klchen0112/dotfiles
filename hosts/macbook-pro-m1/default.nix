3826fe0a230632f297828d87c8edc0bdbed65091
klchen0112 <klchen0112@gmail.com>
2023-11-14 15:23:44 +0800

    remove unused home-manager soft-link

--- hosts/macbook-pro-m1/default.nix
+++ hosts/macbook-pro-m1/default.nix
@@ -34,32 +34,4 @@
   };
 
   programs.home-manager.enable = true;
-  home.activation = {
-    trampolineApps =
-      let
-        apps = pkgs.buildEnv {
-          name = "home-manager-applications";
-          paths = config.home.packages;
-          pathsToLink = "/Applications";
-        };
-      in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        toDir="$HOME/Applications/HMApps"
-        fromDir="${apps}/Applications"
-        rm -rf "$toDir"
-        mkdir "$toDir"
-        (
-          cd "$fromDir"
-          for app in *.app; do
-            /usr/bin/osacompile -o "$toDir/$app" -e "do shell script \"open '$fromDir/$app'\""
-            icon="$(/usr/bin/plutil -extract CFBundleIconFile raw "$fromDir/$app/Contents/Info.plist")"
-            if [[ $icon != *".icns" ]]; then
-              icon="$icon.icns"
-            fi
-            mkdir -p "$toDir/$app/Contents/Resources"
-            cp -f "$fromDir/$app/Contents/Resources/$icon" "$toDir/$app/Contents/Resources/applet.icns"
-          done
-        )
-      '';
-  };
 }
