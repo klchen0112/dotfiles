0c9eb3494c2df096098b9ae928ebc462f2722216
klchen0112 <klchen0112@gmail.com>
2024-01-22 21:39:43 +0800

    use texlive full

--- modules/lang/latex/home.nix
+++ modules/lang/latex/home.nix
@@ -1,11 +1,6 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.texlive = {
     enable = true;
-    extraPackages = tpkgs: {
-      inherit
-        (tpkgs)
-        scheme-full
-        ;
-    };
+    extraPackages = tpkgs: { inherit (tpkgs) scheme-full biblatex latexmk; };
   };
 }
