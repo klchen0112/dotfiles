8a2f833b02053ab780807ceea98710334a0cf5d7
klchen0112 <klchen0112@gmail.com>
2024-02-27 20:04:19 +0800

    remove unused pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,21 +5,6 @@
   pkgs,
 }: {
   # example = pkgs.callPackage ./example { };
-  sf-pro = inputs.apple-fonts.packages.${pkgs.system}.sf-pro;
-  sf-pro-nerd = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
-
-  sf-compact = inputs.apple-fonts.packages.${pkgs.system}.sf-compact;
-  sf-compact-nerd = inputs.apple-fonts.packages.${pkgs.system}.sf-compact-nerd;
-
-  sf-mono = inputs.apple-fonts.packages.${pkgs.system}.sf-mono;
-  sf-mono-nerd = inputs.apple-fonts.packages.${pkgs.system}.sf-mono-nerd;
-
-  sf-arabic = inputs.apple-fonts.packages.${pkgs.system}.sf-arabic;
-  sf-arabic-nerd = inputs.apple-fonts.packages.${pkgs.system}.sf-arabic-nerd;
-
-  ny = inputs.apple-fonts.packages.${pkgs.system}.ny;
-  ny-nerd = inputs.apple-fonts.packages.${pkgs.system}.ny-nerd;
-
   TsangerJinKai02 = inputs.own-nur.packages.${pkgs.system}.TsangerJinKai02;
   Jigmo = inputs.own-nur.packages.${pkgs.system}.Jigmo;
 
