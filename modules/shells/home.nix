6756ee2dd3e4793bfc9ebc318cc8a7eafcbb0983
klchen0112 <klchen0112@gmail.com>
2024-03-26 15:57:59 +0800

    add editorconfig

--- modules/shells/home.nix
+++ modules/shells/home.nix
@@ -330,8 +330,23 @@
     just
     coreutils
     gnugrep
+    python311Packages.editorconfig
   ];
   home.sessionVariables = {
     "EIDTOR" = "emacs";
   };
+  # programs.editorconfig = {
+  #   enable = true;
+  #   settings = {
+  #     "*" = {
+  #       charset = "utf-8";
+  #       end_of_line = "lf";
+  #       trim_trailing_whitespace = true;
+  #       insert_final_newline = true;
+  #       max_line_width = 80;
+  #       indent_style = "space";
+  #       indent_size = 4;
+  #     };
+  #   };
+  # };
 }
--- modules/socialMedia/home.nix
+++ modules/socialMedia/home.nix
@@ -4,5 +4,5 @@
   ...
 }: {
   home.packages = with pkgs;
-    [discord ] ++ lib.optionals (pkgs.stdenv.isLinux) [];
+    [discord] ++ lib.optionals (pkgs.stdenv.isLinux) [];
 }
