fafbe5b67d0d3a7f791e39f83a84b1f5ba90f0d0
chenkailong_dxm <chenkailong@duxiaoman.com>
2024-04-17 13:26:20 +0800

    update firefox config

--- modules/browser/home.nix
+++ modules/browser/home.nix
@@ -13,18 +13,74 @@
       if pkgs.stdenv.isDarwin
       then pkgs.nur.repos.toonn.apps.firefox
       else pkgs.firefox-wayland; # firefox with wayland support
-    profiles = {
-      "klchen" = {
-        id = 0;
-        settings = {};
-        search = {
-          force = true;
-          default = "DuckDuckGo";
-          order = ["DuckDuckGo" "Google"];
-        };
-        # extensions = with
+    profiles.default = {
+      id = 0;
+      isDefault = true;
+      name = "klchen";
+
+      # settings = {};
+      search = {
+        force = true;
+        default = "Bing";
+        order = ["Bing" "Baidu" "DuckDuckGo" "Google"];
       };
+      settings = {
+        "browser.tabs.loadInBackground" = true;
+        #   "widget.gtk.rounded-bottom-corners.enabled" = true;
+        #   "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        #   "svg.context-properties.content.enabled" = true;
+        bookmarks = [
+          {
+            name = "wikipedia";
+            tags = ["wiki"];
+            keyword = "wiki";
+            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+          }
+          {
+            name = "kernel.org";
+            url = "https://www.kernel.org";
+          }
+          {
+            name = "Nix sites";
+            bookmarks = [
+              {
+                name = "Home Manager";
+                url = "https://nixos.wiki/wiki/Home_Manager";
+              }
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                tags = ["wiki" "nix"];
+                url = "https://nixos.wiki/";
+              }
+              {
+                name = "mynixios";
+                tags = ["wiki" "nix"];
+                url = "https://mynixos.com/";
+              }
+            ];
+          }
+        ];
+      };
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        # see: https://github.com/nix-community/nur-combined/blob/master/repos/rycee/pkgs/firefox-addons/generated-firefox-addons.nix
+        gopass-bridge
+        # https-everywhere
+        link-cleaner
+        privacy-badger
+        tree-style-tab
+        multi-account-containers
+        # ublock-origin
+        # vimium
+        sidebery
+        bitwarden
+        omnivore
+      ];
     };
+
     # home.packages = with pkgs; [tor];
   };
 }
