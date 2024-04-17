dc5b749156582dac4f85aa51b0036dbe5eb1c5e0
chenkailong_dxm <chenkailong@duxiaoman.com>
2024-04-17 13:01:49 +0800

    update casks

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -17,23 +17,23 @@
       cleanup = "zap"; # Uninstall not listed packages and casks
     };
     global.autoUpdate = false;
-    masApps = {
-
-      onedrive = 823766827;
-      "goodnotes-5" = 1444383602;
-      "Bitwarden" = 1352778147;
-      # xcode = 497799835;
-    }
-    # // (lib.optionals !isWork){
-    #   wechat = 836500024;
-    #   qq = 451108668;
-    #   dingtalk = 1435447041;
-    #   #. tencent-meeting = 1484048379;
-    #   "microsoft-word" = 462054704;
-    #   "microsoft-powerpoint" = 462062816;
-    #   "microsoft-excel" = 462058435;
-    # }
-    ;
+    masApps =
+      {
+        onedrive = 823766827;
+        "goodnotes-5" = 1444383602;
+        "Bitwarden" = 1352778147;
+        # xcode = 497799835;
+      }
+      # // (lib.optionals !isWork){
+      #   wechat = 836500024;
+      #   qq = 451108668;
+      #   dingtalk = 1435447041;
+      #   #. tencent-meeting = 1484048379;
+      #   "microsoft-word" = 462054704;
+      #   "microsoft-powerpoint" = 462062816;
+      #   "microsoft-excel" = 462058435;
+      # }
+      ;
 
     taps = builtins.attrNames config.nix-homebrew.taps;
     brews = [
@@ -41,60 +41,62 @@
       "mas"
     ];
     caskArgs = {no_quarantine = true;};
-    casks = [
-      "squirrel"
-      "anki"
-      "snipaste"
-      "google-chrome"
+    casks =
+      [
+        "squirrel"
+        "anki"
+        "snipaste"
+        "google-chrome"
 
-      "appcleaner"
+        "appcleaner"
 
-      "baidunetdisk"
-      "iterm2"
+        "baidunetdisk"
+        "iterm2"
 
-      "keyboardcleantool"
+        "keyboardcleantool"
 
-      "vial"
-      "dash"
+        "vial"
+        "dash"
 
-      "omnidisksweeper"
+        "omnidisksweeper"
 
-      "xnviewmp"
-      "zotero"
-      "skim"
+        "xnviewmp"
+        "zotero"
+        "skim"
 
-      # "TidGi"
-      # "mathpix-snipping-tool"
-      "logi-options-plus"
-      "sfm"
-      "marginnote"
-      "raycast"
-      "plexamp"
-      "syncthing"
-      # "mpv"
-      "iina"
-      "licecap"
-    ] ++ lib.optionals (!isWork) [
-      "microsoft-remote-desktop"
-      "sunloginclient"
-      "adrive"
-      "steam"
-      "telegram"
-      "balenaetcher"
-      "todesk"
-      "tencent-meeting"
-      "marginnote"
-      "nutstore"
-      "discord"
-      "openvpn-connect"
-      "activitywatch"
+        # "TidGi"
+        # "mathpix-snipping-tool"
+        "logi-options-plus"
+        "sfm"
+        "marginnote"
+        "raycast"
+        "plexamp"
+        "syncthing"
+        # "mpv"
+        "iina"
+        "licecap"
+      ]
+      ++ lib.optionals (!isWork) [
+        "microsoft-remote-desktop"
+        "sunloginclient"
+        "adrive"
+        "steam"
+        "telegram"
+        "balenaetcher"
+        "todesk"
+        "tencent-meeting"
+        "marginnote"
+        "nutstore"
+        "discord"
+        "openvpn-connect"
+        "activitywatch"
 
-      # keyboard
-      "via"
-      "vial"
-      "qmk-toolbox"
-      "openscad"
-      "zerotier-one"
-    ];
+        # keyboard
+        "via"
+        "vial"
+        "qmk-toolbox"
+        "openscad"
+        "zerotier-one"
+      ];
   };
 }
