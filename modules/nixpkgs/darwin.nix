ab4e875285a44cbaf54227f68a8650fdb27cad17
klchen0112 <klchen0112@gmail.com>
2024-04-24 00:02:51 +0800

    nix optimise

--- modules/nixpkgs/darwin.nix
+++ modules/nixpkgs/darwin.nix
@@ -15,6 +15,11 @@
     Hour = 3;
     Minute = 15;
   };
+  nix.optimise.user = "${username}";
+  nix.optimise.interval = {
+    Hour = 3;
+    Minute = 15;
+  };
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   programs.nix-index.enable = true;
   services.nix-daemon.enable = true; # Auto upgrade daemon
