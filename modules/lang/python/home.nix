4500a9e495cf8d968f179fe74db86c5572bf3b85
chenkailong_dxm <chenkailong@duxiaoman.com>
2024-06-13 10:34:30 +0800

    fix pyright

--- modules/lang/python/home.nix
+++ modules/lang/python/home.nix
@@ -5,7 +5,7 @@
   home.packages = with pkgs; [
     # (python310.withPackages (ps: with ps; [isort pyflakes black matplotlib numpy pandas tensorflow torch torchvision virtualenv opencv4 tqdm conda]))
     # python3Full
-    nodePackages.pyright # python language server
+    pyright # python language server
     # python311Packages.black # python formatter
     # micromamba
   ];
