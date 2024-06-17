295f0c0141633c3c86ea9ca38be3a450bd95ee05
chenkailong_dxm <chenkailong@duxiaoman.com>
2024-06-17 12:44:10 +0800

    fix links

--- hosts/mbp-m1/default.nix
+++ hosts/mbp-m1/default.nix
@@ -35,5 +35,16 @@
     stateVersion = "23.11";
   };
 
+  home.activation = {
+    rsync-home-manager-applications = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+      apps_source="$genProfilePath/home-path/Applications"
+      moniker="Home Manager Trampolines"
+      app_target_base="${config.home.homeDirectory}/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
+    '';
+  };
   programs.home-manager.enable = true;
 }
