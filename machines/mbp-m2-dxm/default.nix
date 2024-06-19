5625b71b66946e5b8b35c5d356636812aa88689a
klchen0112 <klchen0112@gmail.com>
2024-06-19 12:32:52 +0800

    remove link

--- machines/mbp-m2-dxm/default.nix
+++ machines/mbp-m2-dxm/default.nix
@@ -50,15 +50,5 @@
     ];
   };
 
-  system.activationScripts.postUserActivation.text = ''
-    rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-    apps_source="${config.system.build.applications}/Applications"
-    moniker="Nix Trampolines"
-    app_target_base="$HOME/Applications"
-    app_target="$app_target_base/$moniker"
-    mkdir -p "$app_target"
-    ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
-  '';
-
   services.activate-system.enable = true;
 }
