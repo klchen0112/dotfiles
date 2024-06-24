1127ea25362aa00e6e3754bef2d516aa7ba53c84
klchen0112 <klchen0112@gmail.com>
2024-06-24 14:05:08 +0800

    move bin

--- modules/editors/emacs/home.nix
+++ modules/editors/emacs/home.nix
@@ -13,39 +13,6 @@
     else pkgs.emacs29-pgtk;
 in {
   imports = [inputs.nix-doom-emacs-unstraightened.hmModule];
-  home.packages = with pkgs;
-    [
-      fd
-      curl
-      sqlite
-
-      # for emacs sqlite
-      gcc
-      # org mode dot
-      graphviz
-      imagemagick
-
-      # mpvi required
-      tesseract5
-      ffmpeg_5
-      ffmpegthumbnailer
-      mediainfo
-      # email
-      # mu4e
-      # spell check
-      aspell
-
-      # for emacs rime
-      librime
-
-      libwebp
-      tdlib
-      pkg-config
-    ]
-    ++ (lib.optionals pkgs.stdenv.isDarwin) [
-      # pngpaste for org mode download clip
-      pngpaste
-    ];
 
   home.file.".config/emacs/Rime" = {
     source = "${inputs.own-rime}";
@@ -71,7 +38,44 @@ in {
     enable = true;
     doomDir = ./doom;
     emacs = emacsPackage;
-    extraPackages = epkgs: [epkgs.vterm epkgs.treesit-grammars.with-all-grammars];
+    extraBinPackages = with pkgs;
+      [
+        ripgrep
+        fd
+        tdlib
+        curl
+        # for emacs sqlite
+        gcc
+        # org mode dot
+        graphviz
+        imagemagick
+        # mpvi required
+        tesseract5
+        ffmpeg_5
+        ffmpegthumbnailer
+        mediainfo
+        # email
+        # mu4e
+        # spell check
+        aspell
+
+        # for emacs rime
+        librime
+
+        libwebp
+        tdlib
+        pkg-config
+      ]
+      ++ (lib.optionals pkgs.stdenv.isDarwin) [
+        # pngpaste for org mode download clip
+        pngpaste
+      ];
+    extraPackages = epkgs:
+      with epkgs; [
+        vterm
+        treesit-grammars.with-all-grammars
+        # rime
+      ];
     provideEmacs = true;
   };
   services.emacs = {
