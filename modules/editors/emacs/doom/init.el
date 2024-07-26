ac69b3fd541ad385b2bd5899981edc5ce943ad58
klchen0112 <klchen0112@gmail.com>
2024-07-26 11:00:27 +0800

    add flymake

--- modules/editors/emacs/doom/init.el
+++ modules/editors/emacs/doom/init.el
@@ -102,7 +102,7 @@
        vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe)              ; tasing you for every semicolon you forget
+       (syntax +childframe +icons +flymake)               ; tasing you for every semicolon you forget
        (spell) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
