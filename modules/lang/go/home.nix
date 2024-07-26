83459551b8c87f6a810458c97ea33c9fdf71c672
klchen0112 <klchen0112@gmail.com>
2024-07-26 19:14:23 +0800

    update go

--- modules/lang/go/home.nix
+++ modules/lang/go/home.nix
@@ -7,5 +7,13 @@
     gore
     gotools
   ];
-  programs.go.enable = true;
+  programs.go = {
+    enable = true;
+    # packages = {
+    # "volcano.sh/apis"= builtins.fetchGit "https://github.com/volcano-sh/apis";
+    # "volcano.sh/volcano" = builtins.fetchGit "https://github.com/volcano-sh/volcano";
+    # "golang.org/x/text" = builtins.fetchGit "https://go.googlesource.com/text";
+    # "golang.org/x/time" = builtins.fetchGit "https://go.googlesource.com/time";
+    # };
+  };
 }
