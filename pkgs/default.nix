c65e5d91c408e4ce2a02b3a8eabad9caf72315b2
klchen0112 <klchen0112@gmail.com>
2024-11-11 13:46:12 +0800

    emacs: add macport

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -135,4 +135,59 @@
     meta.platforms = pkgs.lib.platforms.darwin;
     meta.mainProgram = "emacs";
   });
+  emacs-macport-own =
+    (
+      pkgs.emacs-macport.override
+      {
+        withNativeCompilation = true;
+        withSQLite3 = true;
+        withTreeSitter = true;
+        withWebP = true;
+        withImageMagick = true;
+      }
+    )
+    .overrideAttrs
+    (old: {
+      patches =
+        (old.patches or [])
+        ++ [
+          # Fix OS window role (needed for window managers like yabai)
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+            sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+          })
+          # Enable rounded window with no decoration
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+            sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+          })
+          # Enableno -frame-refocus-cocoa
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/no-frame-refocus-cocoa.patch";
+            sha256 = "QLGplGoRpM4qgrIAJIbVJJsa4xj34axwT3LiWt++j/c=";
+          })
+          # Make Emacs aware of OS-level light/dark mode
+          (pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+            sha256 = "3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
+          })
+          #Cursor animation
+          # (pkgs.fetchpatch {
+          #   url = "https://raw.githubusercontent.com/sincebyte/neo-emacs/master/patches/emacs31/cursor-animation.patch";
+          #   sha256 = "sha256-xrg9iAl0ZrfqzCnq0oact5XozRYOY/rP/MO8QdbIkM0=";
+          # })
+          # alpha-background
+          # (pkgs.fetchpatch {
+          #   url = "https://raw.githubusercontent.com/LuciusChen/.emacs.d/refs/heads/main/patches/ns-alpha-background.patch";
+          #   sha256 = "sha256-sJ74+0jshtt0KwASHrn23ZlIOPr4zy47Dh05UDic62s=";
+          # })
+          # ns-mac-input-source
+          # (pkgs.fetchpatch {
+          #   url = "https://raw.githubusercontent.com/LuciusChen/.emacs.d/refs/heads/main/patches/ns-mac-input-source.patch";
+          #   sha256 = "sha256-E9BR/axZMhA3QTeoHIKU62Rogr7ZmTtWpnYdi69npNM=";
+          # })
+        ];
+      meta.platforms = pkgs.lib.platforms.darwin;
+      meta.mainProgram = "emacs";
+    });
 }
