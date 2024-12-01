0462dbe8f63a00b4d2cb35815f122ee4c3e9775b
klchen0112 <klchen0112@gmail.com>
2024-12-01 13:33:13 +0800

    use jetpack preset

--- modules/shells/home.nix
+++ modules/shells/home.nix
@@ -186,98 +186,12 @@
     enable = true;
     enableZshIntegration = true;
     enableFishIntegration = true;
+    enableTransience = true;
     settings =
-      {
-        add_newline = false;
-        format = lib.concatStrings [
-          "$shlvl"
-          "$shell"
-          "$username"
-          "$hostname"
-          "$directory"
-          "$git_branch"
-          "$git_commit"
-          "$git_state"
-          "$git_status"
-          "$cmake"
-          "$python"
-          "$nix_shell"
-          "$cmd_duration"
-          "$sudo"
-          "$line_break"
-          "$jobs"
-          "$character"
-        ];
-        shlvl = {
-          disabled = false;
-          style = "bright-red bold";
-        };
-        shell = {
-          disabled = false;
-          format = "$indicator";
-          fish_indicator = "";
-          bash_indicator = "[BASH](bright-black) ";
-          zsh_indicator = "[ZSH](bright-black) ";
-        };
-        username = {
-          disabled = false;
-          style_user = "bright-white bold";
-          style_root = "bright-red bold";
-        };
-        hostname = {
-          disabled = false;
-          style = "bright-green bold";
-          ssh_only = true;
-        };
-        cmd_duration = {
-          disabled = false;
-          format = "⏱ $duration";
-        };
-        git_branch = {
-          disabled = false;
-          format = "[$symbol $branch]($style) ";
-          symbol = "שׂ";
-          style = "bright-yellow bold";
-        };
-        conda = {
-          disabled = false;
-          format = "[$symbol$environment](dimmed green) ";
-        };
-        git_commit = {
-          disabled = false;
-          only_detached = true;
-          format = "[$hash]($style) ";
-          style = "bright-yellow bold";
-        };
-        git_state = {
-          disabled = false;
-          style = "bright-purple bold";
-        };
-        git_status = {disabled = false;};
-        sudo = {
-          disabled = false;
-          format = "as $symbol";
-        };
-        python = {
-          disabled = false;
-          format = "[$pyenv_prefix]($version)(($virtualenv))";
-        };
-        nix_shell = {
-          disabled = false;
-          format = "[$name]($style)";
-        };
-        nodejs = {
-          disabled = false;
-          format = "($version)";
-        };
-        cmake = {
-          disabled = false;
-          format = "($version)";
-        };
-      }
-      // builtins.fromTOML (builtins.readFile "${
+      builtins.fromTOML (builtins.readFile "${
         inputs.own-nur.packages.${pkgs.system}.catppuccin-starship
-      }/palettes/latte.toml");
+      }/palettes/latte.toml")
+      // builtins.fromTOML (builtins.readFile ./jetpack.toml);
   };
   programs.ripgrep = {enable = true;};
 
