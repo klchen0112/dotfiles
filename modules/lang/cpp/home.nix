5505f9a67357477906f286be26e59b548c2a9393
klchen0112 <klchen0112@gmail.com>
2024-12-23 21:40:39 +0800

    cpp: remove packages

--- modules/lang/cpp/home.nix
+++ modules/lang/cpp/home.nix
@@ -2,12 +2,12 @@
   home.packages = with pkgs; [
     #-- c/c++
     cmake
-    cmake-language-server
+    # cmake-language-server
     gnumake
     checkmake
-    gcc # c/c++ compiler, required by nvim-treesitter!
-    llvmPackages.clang-unwrapped # c/c++ tools with clang-tools such as clangd
+    # gcc # c/c++ compiler, required by nvim-treesitter!
+    # llvmPackages.clang-unwrapped # c/c++ tools with clang-tools such as clangd
     # gdb
-    lldb
+    # lldb
   ];
 }
