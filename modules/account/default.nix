ab21b0a4050f9ee32da6d8e396f90829303cc592
klchen0112 <klchen0112@gmail.com>
2025-02-10 00:27:22 +0800

    add keys

--- modules/account/default.nix
+++ modules/account/default.nix
@@ -17,6 +17,8 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGszCNQqxT1/s6sYjj1aewvCjaa3D7UwoOM7UD5K+ha"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyFiios12VBgf9kF4iuOU1VYQhwKz1wpUyADZAnyVGU"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/c10VIo81cztYJza3e+l1JlwsTJQk1lhBOypGhYn3T"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx1SNaQZ6v1onDSGz1wNX1W3zIf2KkTERjKGC+k157D"
+
       ];
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
