0328a8d3d80c9c2ffb1ac1c4def198be5c9fdcc8
klchen0112 <klchen0112@gmail.com>
2025-02-10 00:42:47 +0800

    add keys for root

--- modules/account/default.nix
+++ modules/account/default.nix
@@ -8,18 +8,21 @@
   isWork ? true,
   ...
 }:
+let
+  authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGszCNQqxT1/s6sYjj1aewvCjaa3D7UwoOM7UD5K+ha"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyFiios12VBgf9kF4iuOU1VYQhwKz1wpUyADZAnyVGU"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/c10VIo81cztYJza3e+l1JlwsTJQk1lhBOypGhYn3T"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx1SNaQZ6v1onDSGz1wNX1W3zIf2KkTERjKGC+k157D"
+
+  ];
+in
 {
   users.users.${username} =
     {
       home = if pkgs.stdenv.isLinux then "/home/${username}" else "/Users/${username}";
 
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGszCNQqxT1/s6sYjj1aewvCjaa3D7UwoOM7UD5K+ha"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyFiios12VBgf9kF4iuOU1VYQhwKz1wpUyADZAnyVGU"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/c10VIo81cztYJza3e+l1JlwsTJQk1lhBOypGhYn3T"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx1SNaQZ6v1onDSGz1wNX1W3zIf2KkTERjKGC+k157D"
-
-      ];
+      openssh.authorizedKeys.keys = authorizedKeys;
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
       isNormalUser = true;
@@ -30,4 +33,7 @@
         "seat"
       ];
     };
+}
+// lib.optionalAttrs pkgs.stdenv.isLinux {
+  users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
 }
