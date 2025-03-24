4f3ff1737f23b13bdebe7ee817d40d5512c42000
klchen0112 <klchen0112@gmail.com>
2025-03-24 23:48:59 +0800

    fix nixpkgs

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -24,7 +24,7 @@
     !include ${config.age.secrets.access-tokens.path}
   '';
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = nixpkgs;
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
   # https://github.com/NixOS/nix/issues/9574
