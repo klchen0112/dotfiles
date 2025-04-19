13f7b6c87774430987ecb5369aacae32eb4cb5c3
klchen0112 <klchen0112@gmail.com>
2025-04-20 01:42:02 +0800

    set latex live

--- modules/lang/latex/home.nix
+++ modules/lang/latex/home.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 {
-  # programs.texlive = {
-  #   enable = true;
-  #   packageSet = pkgs.texliveFull;
-  #   # packages = pkgs.texliveFull;
-  #   # extraPackages = pkgs.texliveFull;
-  # };
+  programs.texlive = {
+    enable = true;
+    # packages = pkgs.texliveFull;
+    extraPackages = tpkgs: {
+      inherit (tpkgs) collection-basic;
+      inherit (tpkgs) collection-latex;
+      inherit (tpkgs) collection-latexrecommended;
+      inherit (tpkgs) collection-latexextra;
+      inherit (tpkgs) collection-luatex;
+      inherit (tpkgs) collection-fontsrecommended;
+      inherit (tpkgs) collection-fontsextra;
+    };
+  };
 }
