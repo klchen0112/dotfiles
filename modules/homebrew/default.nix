80e2be55fa72e3e66e6c6adac75a7d04f6dc66ef
klchen0112 <klchen0112@gmail.com>
2025-04-21 21:15:32 +0800

    use brew packages

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -13,63 +13,6 @@
     inputs.brew-nix.darwinModules.default
   ];
   brew-nix.enable = true;
-  environment.systemPackages =
-    with inputs.brew-nix.packages.${pkgs.system};
-    [
-      # sf-symbols
-      # squirrel
-      anki
-      snipaste
-      # google-chrome
-
-      appcleaner
-
-      #baidunetdisk
-
-      # keyboardcleantool
-
-
-      omnidisksweeper
-
-      # xnviewmp
-
-      zotero
-      # zerotier-one
-      # TidGi
-      # mathpix-snipping-tool
-      # "logi-options-plus"
-      # marginnote
-      raycast
-      # sol
-
-    ]
-    ++ lib.optionals isWork [
-      # iterm2
-    ]
-    ++ lib.optionals (!isWork) [
-      # syncthing
-      # sunloginclient
-      # adrive
-      # steam
-      # balenaetcher
-      # todesk
-      # marginnote
-      # nutstore
-      discord
-      # openvpn-connect
-      activitywatch
-      # sfm
-      plexamp
-      # keyboard
-      qmk-toolbox
-      # openscad
-      vial
-
-      # vpn
-      # tailscale
-
-      telegram
-    ];
   homebrew = {
     # Declare Homebrew using Nix-Darwin
     enable = true;
@@ -81,23 +24,22 @@
     global.autoUpdate = false;
     masApps =
       {
-       
+
         "Bitwarden" = 1352778147;
-       
-       
+
         # dingtalk = 1435447041;
         xcode = 497799835;
-       
+
       }
       // (
         if isWork then
           { }
         else
           {
-             "goodnotes-5" = 1444383602;
-             qq = 451108668;
-             wechat = 836500024;
-             spark = 1176895641;
+            "goodnotes-5" = 1444383602;
+            qq = 451108668;
+            wechat = 836500024;
+            spark = 1176895641;
             # onedrive = 823766827;
             # tencent-meeting = 1484048379;
             "microsoft-word" = 462054704;
@@ -106,47 +48,60 @@
           }
       );
 
-    brews = [
-      # "macism
-      #"sleepwatcher"
-      #"musl-cross"
-      # "micromamba"
-      "syncthing"
-      
-    ] ++ lib.optionals isWork [
-      "podman"
-      "podman-compose"
-      "podman-tui"
+    brews =
+      [
+        # "macism
+        #"sleepwatcher"
+        #"musl-cross"
+        # "micromamba"
+        "syncthing"
 
-    ];
+      ]
+      ++ lib.optionals isWork [
+        "podman"
+        "podman-compose"
+        "podman-tui"
+
+      ];
     taps = [
       # "homebrew/cask"
       # "homebrew/services"
     ];
     casks =
       [
+        "anki"
+        "snipaste"
+        "appcleaner"
         "microsoft-remote-desktop"
         "google-chrome"
         "logi-options+"
-        "firefox"
+        # "firefox"
         "font-sf-pro"
         "font-sf-mono"
         "zen-browser"
         "podman-desktop"
         "squirrel"
-      # mpv
-      "iina"
-      "skim"
-
-      "licecap"
-      # Keyboard
-      # hhkbStudio
-      # microsoft-remote-desktop
+        # mpv
+        "iina"
+        "skim"
+        "zotero"
+        "omnidisksweeper"
+        "licecap"
+        "raycast"
+        # Keyboard
+        # hhkbStudio
+        # microsoft-remote-desktop
         # "syncthing"
       ]
       ++ lib.optionals (!isWork) [
         "steam"
         "adrive"
+        "discord"
+        "activitywatch"
+        "vial"
+        "qmk-toolbox"
+        "plexamp"
+        "telegram"
       ]
       ++ lib.optionals isWork [
         "iterm2"
