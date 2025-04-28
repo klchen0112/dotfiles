a9b530a163eb25103006832803f96ea41d05e456
klchen0112 <klchen0112@gmail.com>
2025-04-28 14:35:56 +0800

    fix neovim

--- modules/editors/vim/home.nix
+++ modules/editors/vim/home.nix
@@ -106,9 +106,6 @@
 
       # If you want to automatically add `(` after selecting a function or method
       # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraconfiglua#extraconfiglua
-      extraConfigLua = ''
-        require('cmp').event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
-      '';
       # Options.
       opts = {
         # Show line numbers
