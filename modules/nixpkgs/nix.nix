b6e4b0202756d5acd606cdaf43b75085a7ca4159
klchen0112 <klchen0112@gmail.com>
2025-05-03 17:36:30 +0800

    update substituters

--- modules/nixpkgs/nix.nix
+++ modules/nixpkgs/nix.nix
@@ -16,21 +16,22 @@
     substituters = [
       # replace official cache with a mirror located in China
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
-      "https://colmena.cachix.org"
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
-    ];
-    extra-substituters = [
       "https://klchen0112.cachix.org"
       "https://cosmic.cachix.org"
+
+      "https://colmena.cachix.org"
       "https://cache.garnix.io"
     ];
-    extra-trusted-public-keys = [
-      "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
+    trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+
     ];
 
     trusted-users = [
