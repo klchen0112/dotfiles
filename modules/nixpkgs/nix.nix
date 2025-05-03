b1fd58f69ed77899c98ec55e17b6b59e3ab157eb
klchen0112 <klchen0112@gmail.com>
2025-05-03 17:52:28 +0800

    fix substituters

--- modules/nixpkgs/nix.nix
+++ modules/nixpkgs/nix.nix
@@ -15,17 +15,18 @@
     extra-experimental-features = "nix-command flakes";
     substituters = [
       # replace official cache with a mirror located in China
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
-      # "https://mirror.sjtu.edu.cn/nix-channels/store"
+    ];
+    extra-substituters = [
       "https://nix-community.cachix.org"
       "https://klchen0112.cachix.org"
       "https://cosmic.cachix.org"
-
       "https://colmena.cachix.org"
       "https://cache.garnix.io"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
