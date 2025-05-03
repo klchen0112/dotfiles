b40c287bda618d93e9daaf9024da7a1c0d94bbf9
klchen0112 <klchen0112@gmail.com>
2025-05-03 14:29:47 +0800

    add colemna

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
     ];
 
     trusted-users = [
