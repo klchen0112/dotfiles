aa90e99dc23a402633766e15adbe9e43396f2cf8
klchen0112 <klchen0112@gmail.com>
2025-05-06 23:05:05 +0800

    move cosmic

--- modules/desktop/cosmic/default.nix
+++ modules/desktop/cosmic/default.nix
@@ -32,4 +32,9 @@
     quick-webapps
   ];
 
+  nix.settings = {
+    substituters = [ "https://cosmic.cachix.org/" ];
+    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+  };
+
 }
