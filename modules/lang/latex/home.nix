26f8e36f6d5c4f7f71ee861be32289373e6e84fa
klchen0112 <klchen0112@gmail.com>
2025-05-10 21:34:42 +0800

    update latex settings

--- modules/lang/latex/home.nix
+++ modules/lang/latex/home.nix
@@ -3,14 +3,20 @@
   programs.texlive = {
     enable = true;
     # packages = pkgs.texliveFull;
-    extraPackages = tpkgs: {
-      inherit (tpkgs) collection-basic;
-      inherit (tpkgs) collection-latex;
-      inherit (tpkgs) collection-latexrecommended;
-      inherit (tpkgs) collection-latexextra;
-      inherit (tpkgs) collection-luatex;
-      inherit (tpkgs) collection-fontsrecommended;
-      inherit (tpkgs) collection-fontsextra;
-    };
+    extraPackages = tp: { inherit (tp)
+      scheme-full
+      collection-latexrecommended
+      collection-xetex
+
+      aligned-overset
+      blindtext
+      catchfile
+      datetime2
+      enumitem
+      multirow
+      spreadtab
+      tracklang
+      xstring
+    ;};
   };
 }
