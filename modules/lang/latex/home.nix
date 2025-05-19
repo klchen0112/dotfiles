63dff0def132a3440ce0666c0eaa5e939ac42754
klchen0112 <klchen0112@gmail.com>
2025-05-19 10:29:08 +0800

    fmt files

--- modules/lang/latex/home.nix
+++ modules/lang/latex/home.nix
@@ -3,20 +3,22 @@
   programs.texlive = {
     enable = true;
     # packages = pkgs.texliveFull;
-    extraPackages = tp: { inherit (tp)
-      scheme-full
-      collection-latexrecommended
-      collection-xetex
+    extraPackages = tp: {
+      inherit (tp)
+        scheme-full
+        collection-latexrecommended
+        collection-xetex
 
-      aligned-overset
-      blindtext
-      catchfile
-      datetime2
-      enumitem
-      multirow
-      spreadtab
-      tracklang
-      xstring
-    ;};
+        aligned-overset
+        blindtext
+        catchfile
+        datetime2
+        enumitem
+        multirow
+        spreadtab
+        tracklang
+        xstring
+        ;
+    };
   };
 }
