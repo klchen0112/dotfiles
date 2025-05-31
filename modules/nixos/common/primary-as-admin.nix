f9bd57da97d1f914b2be114f91919f4604b187fc
klchen0112 <klchen0112@gmail.com>
2025-06-01 02:50:06 +0800

    move account

---
+++ modules/nixos/common/primary-as-admin.nix
@@ -0,0 +1,31 @@
+# Make flake.config.peope.myself the admin of the machine
+{ flake, pkgs, lib, ... }:
+
+{
+  # Login via SSH with mmy SSH key
+  users.users =
+    let
+      me = flake.config.me;
+      myKeys = [
+        me.sshKey
+      ];
+    in
+    {
+      root.openssh.authorizedKeys.keys = myKeys;
+      ${me.username} = {
+        openssh.authorizedKeys.keys = myKeys;
+        shell = pkgs.bash;
+      } // lib.optionalAttrs pkgs.stdenv.isLinux {
+        isNormalUser = true;
+        extraGroups = [ "networkmanager" "wheel" ];
+      };
+    };
+
+  programs.bash.enable = lib.mkIf pkgs.stdenv.isLinux true;
+
+  # Make me a sudoer without password
+  security = lib.optionalAttrs pkgs.stdenv.isLinux {
+    sudo.execWheelOnly = true;
+    sudo.wheelNeedsPassword = false;
+  };
+}
