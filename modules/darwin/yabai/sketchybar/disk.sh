9f9fa69bdb029bab9e1d1646ef885d594ee6ee1b
klchen0112 <klchen0112@gmail.com>
2025-06-13 03:11:00 +0800

    remove yabai

--- modules/darwin/yabai/sketchybar/disk.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-used_disk_percentage=$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf ("%s\n", $5) }')
-
-sketchybar --set {NAME} label="$used_disk_percentage"
--- modules/darwin/yabai/sketchybar/mem.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-used_memory_percentage=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')
-
-sketchybar --set $NAME label="$used_memory_percentage%"
