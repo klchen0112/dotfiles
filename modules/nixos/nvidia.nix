a6d2b5a9518cdb53fc2c33cee738b857f48d9128
klchen0112 <klchen0112@gmail.com>
2025-06-14 00:41:37 +0800

    fix nvidia

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -1,13 +1,20 @@
-{ flake, config, ... }:
+{ config, ... }:
 {
-  # Enable OpenGL
+  boot.initrd.kernelModules = [ "nvidia" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  boot.kernelParams = [
+    "ibt=off"
+  ];
+
   hardware.graphics = {
     enable = true;
   };
+
+  # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
-  # nvidia settings
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
+
   hardware.nvidia = {
+
     # Modesetting is required.
     modesetting.enable = true;
 
@@ -27,13 +34,14 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
+
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
-
 }
