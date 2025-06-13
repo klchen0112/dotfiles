efc5ffd227d545fd8b382f6228e86f45c5baa151
klchen0112 <klchen0112@gmail.com>
2025-06-13 22:36:50 +0800

    update nvidia

--- configurations/nixos/i12r20/default.nix
+++ configurations/nixos/i12r20/default.nix
@@ -5,6 +5,7 @@ in
 {
   imports = [
     flake.inputs.self.nixosModules.default
+    flake.inputs.self.nixosModules.linux.nvidia
     ./hardware-configuration.nix
   ];
   machine = machine;
@@ -28,44 +29,11 @@ in
   # persist this file you won't be able to easily use journalctl to
   # look at journals for previous boots.
   environment.etc."machine-id".source = "/nix/persist/etc/machine-id";
-  services.xserver.videoDrivers = [ "nvidia" ];
-  # nvidia settings
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
   environment.etc."ssh/ssh_host_rsa_key".source = "/nix/persist/etc/ssh/ssh_host_rsa_key";
   environment.etc."ssh/ssh_host_rsa_key.pub".source = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
   environment.etc."ssh/ssh_host_ed25519_key".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
   environment.etc."ssh/ssh_host_ed25519_key.pub".source =
     "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
+
 }
