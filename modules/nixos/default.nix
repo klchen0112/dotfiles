a2b8d5f30e90abc8ccd40eb2b74a31a7182b9292
klchen0112 <klchen0112@gmail.com>
2025-06-14 16:15:02 +0000

    fix nixos stylix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,7 +6,7 @@
     ./common
     ./linux/access-tokens.nix
     ./linux/fonts
-    ./stylix.nix
+    ./common/stylix.nix
     flake.inputs.stylix.nixosModules.stylix
   ];
   services.openssh.enable = true;
