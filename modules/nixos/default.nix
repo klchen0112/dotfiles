7d80d734b46455eafef972cced1de1ca3138805e
klchen0112 <klchen0112@gmail.com>
2025-06-15 01:04:03 +0800

    move nixos modules

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -4,9 +4,9 @@
   :{
   imports = [
     ./common
-    ./linux/access-tokens.nix
-    ./linux/fonts
-    ./common/stylix.nix
+    ./access-tokens.nix
+    ./fonts
+    ./stylix.nix
     flake.inputs.stylix.nixosModules.stylix
   ];
   services.openssh.enable = true;
