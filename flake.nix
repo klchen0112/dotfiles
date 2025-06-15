3f5558517778cd8cffef847f5e8691bda2ab615f
klchen0112 <klchen0112@gmail.com>
2025-06-15 18:05:28 +0000

    update flake.lcok

--- flake.nix
+++ flake.nix
@@ -1,39 +1,5 @@
 {
   description = "My Personal NixOS and Darwin System Flake Configuration";
-  nixConfig = {
-    extra-experimental-features = "nix-command flakes";
-    substituters = [
-      # replace official cache with a mirror located in China
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      # "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
-
-    ];
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://klchen0112.cachix.org"
-      "https://cosmic.cachix.org"
-
-      "https://colmena.cachix.org"
-      "https://cache.garnix.io"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
-      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-
-    ];
-    trusted-users = [
-      "root"
-      "@wheel"
-      "chenkailong_dxm"
-      "klchen"
-    ];
-    accept-flake-config = true;
-    # extra-experimental-features = "nix-command flakes";
-  };
 
   # Wired using https://nixos-unified.org/autowiring.html
   outputs =
@@ -192,13 +158,12 @@
         # inputs.rust-overlay.follows = "rust-overlay";
       };
 
-      # nixos-cosmic = {
-      #   url = "github:lilyinstarlight/nixos-cosmic";
-      #   inputs.nixpkgs.follows = "nixpkgs";
-      #   inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-      #   inputs.flake-compat.follows = "flake-compat";
-      #   inputs.rust-overlay.follows = "rust-overlay";
-      # };
+      nixos-cosmic = {
+        url = "github:lilyinstarlight/nixos-cosmic";
+        inputs.nixpkgs.follows = "nixpkgs";
+        inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+        inputs.flake-compat.follows = "flake-compat";
+      };
       # theme
       stylix = {
         url = "github:danth/stylix";
