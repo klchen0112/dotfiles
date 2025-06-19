71c328914a54823137798b173c1f12720e031a9b
klchen0112 <klchen0112@gmail.com>
2025-06-19 19:24:20 +0800

    add caches

--- modules/home/nix/caches.nix
+++ modules/home/nix/caches.nix
@@ -8,6 +8,7 @@
       "https://klchen0112.cachix.org"
       "https://colmena.cachix.org"
       "https://cache.garnix.io"
+      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
--- modules/nixos/common/caches.nix
+++ modules/nixos/common/caches.nix
@@ -4,6 +4,7 @@
       # replace official cache with a mirror located in China
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       # "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://klchen0112.cachix.org"
       "https://colmena.cachix.org"
