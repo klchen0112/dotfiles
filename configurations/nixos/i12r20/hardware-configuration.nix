a92d2a5df5c0909ea6d7c56f4965ddb686ea4cf9
klchen0112 <klchen0112@gmail.com>
2025-06-21 09:40:32 +0000

    fix i12r20 hardware

--- configurations/nixos/i12r20/hardware-configuration.nix
+++ configurations/nixos/i12r20/hardware-configuration.nix
@@ -29,7 +29,11 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "defaults" "size=2G" "mode=755" ];
+    options = [
+      "defaults"
+      "size=2G"
+      "mode=755"
+    ];
   };
 
   fileSystems."/nix" = {
