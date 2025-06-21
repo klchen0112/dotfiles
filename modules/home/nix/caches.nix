de21d2f572a8dd18b98fdd7b1098ce05e5144152
klchen0112 <klchen0112@gmail.com>
2025-06-21 10:27:35 +0000

    add cache nixos

--- modules/home/nix/caches.nix
+++ modules/home/nix/caches.nix
@@ -11,6 +11,7 @@
       "https://cache.nixos.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
