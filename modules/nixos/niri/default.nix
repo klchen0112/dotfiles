c707662bac3905e1168f0211560c84f64364b2ad
klchen0112 <klchen0112@gmail.com>
2025-06-24 01:42:38 +0800

    enable niri

--- modules/nixos/niri/default.nix
+++ modules/nixos/niri/default.nix
@@ -1,12 +1,30 @@
 {
   pkgs,
   flake,
+  config,
   ...
 }:
 {
   imports = [
     flake.inputs.niri.nixosModules.niri
   ];
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --remember-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+      };
+    };
+  };
+  # polkit agent
+  security.soteria.enable = true;
+
+  # 磁盘挂载
+  services.gvfs.enable = true;
+
+  # 压缩解压
+  programs.file-roller.enable = true;
+
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
   nixpkgs.overlays = [ flake.inputs.niri.overlays.niri ];
