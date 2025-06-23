85aaf479e055da7bc78e2e70338303162c9e6b76
klchen0112 <klchen0112@gmail.com>
2025-06-24 01:37:09 +0800

    fix niri

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -17,7 +17,36 @@
     slurp
     # utils
     wl-clipboard
+    swaybg
+    hyprlock
+    swaylock-effects
+    swayidle
+    wlogout
+    wlsunset
+    fuzzel
+    waybar
+    uwsm
+    xwayland-satellite
+    adwaita-icon-theme
+    gnome-themes-extra
   ];
+  # 调整亮度音量显示
+  services.avizo.enable = true;
+
+  # 通知显示
+  services.swaync.enable = true;
+
+  # 根据不同的设备加载不同的显示器分辨率刷新率缩放
+  # 就不用去 wm 里面一个一个配，导致每次换设备都要重写配置
+  # https://wiki.archlinux.org/title/Kanshi
+  services.kanshi.enable = true;
+
+  # 夜光护眼软件
+  services.wlsunset = {
+    enable = true;
+    latitude = "30:00";
+    longitude = "120:00";
+  };
   nix.settings = {
     # add binary caches
     trusted-public-keys = [
