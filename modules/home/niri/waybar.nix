59d0b9f4ba9e70ea185ea3f4605a0449d35e9531
klchen0112 <klchen0112@gmail.com>
2025-06-29 19:22:19 +0000

    add waybar

--- modules/home/niri/waybar.nix
+++ modules/home/niri/waybar.nix
@@ -5,34 +5,15 @@
   ...
 }:
 {
-  stylix.targets.waybar.enable = true;
+  stylix.targets.waybar = {
+    enable = true;
+    enableLeftBackColors = true;
+    enableRightBackColors = true;
+    enableCenterBackColors = true;
+  };
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    style = lib.mkAfter "
-    @define-color text           #${config.lib.stylix.colors.base05};
-    @define-color red            #${config.lib.stylix.colors.base08};
-    @define-color orange         #${config.lib.stylix.colors.base09};
-    @define-color yellow         #${config.lib.stylix.colors.base0A};
-    @define-color green          #${config.lib.stylix.colors.base0B};
-    @define-color cyan           #${config.lib.stylix.colors.base0C};
-    @define-color blue           #${config.lib.stylix.colors.base0D};
-    @define-color magenta        #${config.lib.stylix.colors.base0E};
-    @define-color brown          #${config.lib.stylix.colors.base0F};
-    @define-color bright-red     #${config.lib.stylix.colors.base12};
-    @define-color bright-yellow  #${config.lib.stylix.colors.base0A};
-    @define-color bright-green   #${config.lib.stylix.colors.base0B};
-    @define-color bright-cyan    #${config.lib.stylix.colors.base0C};
-    @define-color bright-blue    #${config.lib.stylix.colors.base0D};
-    @define-color bright-magenta #${config.lib.stylix.colors.base0E};
-    #waybar {
-      /* background: rgba(17, 17, 17, 0.5); */
-      background: transparent;
-      color: @text;
-      border-radius: 0px 5px 5px 18px;
-    }
-
-    ";
     settings = {
       mainBar = {
         layer = "top";
