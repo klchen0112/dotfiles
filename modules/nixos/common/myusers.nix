d537a1cbc0010e84b20c1052366eabdfe58df45c
klchen0112 <klchen0112@gmail.com>
2025-07-06 14:29:17 +0000

    add hm backup

--- modules/nixos/common/myusers.nix
+++ modules/nixos/common/myusers.nix
@@ -101,6 +101,7 @@ in
             openssh.authorizedKeys.keys = builtins.concatLists rootSshKeys;
           };
       };
+    home-manager.backupFileExtension = "hmbackup";
     # Enable home-manager for our user
     home-manager.users = mapListToAttrs config.myusers (name: {
       imports =
