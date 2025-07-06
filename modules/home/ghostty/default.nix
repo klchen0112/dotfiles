56a75eb985800135d320991cd29e1afe4ff44d89
klchen0112 <klchen0112@gmail.com>
2025-07-06 22:13:55 +0800

    fix ghostty settings

--- modules/home/ghostty/default.nix
+++ modules/home/ghostty/default.nix
@@ -8,6 +8,10 @@
     enable = true;
     package = if pkgs.stdenv.isDarwin then pkgs.brewCasks.ghostty else pkgs.ghostty;
     installBatSyntax = true;
+    settings = {
+      window-colorspace = if pkgs.stdenv.isDarwin then "display-p3" else "srgb";
+      macos-titlebar-style = "hidden";
+    };
   };
 
 }
