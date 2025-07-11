3413caa81ad322b371fa092225871352f230c78b
klchen0112 <klchen0112@gmail.com>
2025-07-11 15:18:47 +0800

    add lua fmt

--- modules/flake/devshell.nix
+++ modules/flake/devshell.nix
@@ -37,6 +37,7 @@
         programs.ruff.check = true;
         programs.ruff.format = true;
         programs.toml-sort.enable = true;
+        programs.stylua.enable = true;
         settings.formatter.shellcheck.options = [
           "-s"
           "bash"
