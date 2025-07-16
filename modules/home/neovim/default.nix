e3f18c7813ef6539b3e4be50d3d37d35f5ef5feb
klchen0112 <klchen0112@gmail.com>
2025-07-16 16:27:12 +0800

    neovim: add treesitter

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -89,8 +89,65 @@
         eob = " ";
       };
     };
-    extraPackages = with pkgs; [
-      nixd
-    ];
+    plugins.treesitter = {
+      enable = true;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        # Linux
+        bash
+        ssh_config
+        # sway
+        tmux
+
+        # Nix, Nixvim
+        nix
+        # query # treesitter queries
+        vim
+        vimdoc
+        lua
+        # luadoc
+
+        # General Development
+        csv
+        diff
+        editorconfig
+        git_config
+        git_rebase
+        gitattributes
+        gitcommit
+        gitignore
+        ini
+        # llvm
+        markdown
+        markdown_inline
+        regex
+        # xml
+        yaml
+
+        # Rust Development
+        # rust
+        toml # Also for ZMK `keymap.toml`
+
+        # Web Development
+        css
+        html
+        # http
+        # javascript
+        json
+        json5
+        # php
+        # php_only
+        # phpdoc
+        # sql
+        # scss
+        # twig
+        # tsx
+        # typescript
+
+        # Web - other
+        # astro
+        # nginx
+        # svelte
+      ];
+    };
   };
 }
