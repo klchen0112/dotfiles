d295c950785b3c1d7f4a743d12cbf5cf41e6982b
klchen0112 <klchen0112@gmail.com>
2025-07-20 13:42:21 +0800

    ghostty: fix copy-on-select

--- modules/home/ghostty/default.nix
+++ modules/home/ghostty/default.nix
@@ -15,7 +15,7 @@
       command = "/etc/profiles/per-user/${config.me.username}/bin/nu";
       clipboard-read = "allow";
       clipboard-write = "allow";
-      copy-on-select = true;
+      copy-on-select = "clipboard";
     };
   };
 
