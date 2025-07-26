57106e9f4f9f0fce4100b8412ac38b9f65bcb7e0
klchen0112 <klchen0112@gmail.com>
2025-07-26 17:40:34 +0000

    add caches for nix-community

--- modules/home/nix/caches.nix
+++ modules/home/nix/caches.nix
@@ -4,6 +4,7 @@
       # replace official cache with a mirror located in China
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
       "https://klchen0112.cachix.org"
     ];
     trusted-substituters = [
@@ -14,6 +15,8 @@
     trusted-public-keys = [
       "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
     ];
   };
 
