86bcd505c1e18ffb23a530c9609bf88ea29a2750
klchen0112 <klchen0112@gmail.com>
2025-08-18 17:51:08 +0000

    remove walker

--- modules/home/niri/walker/default.nix
+++ modules/home/niri/walker/default.nix
@@ -1,17 +1,6 @@
 { pkgs, flake, ... }:
 {
-  imports = [ flake.inputs.walker.homeManagerModules.default ];
   services.walker = {
     enable = true;
-    runAsService = true;
-  };
-  nix.settings = {
-    # add binary caches
-    trusted-public-keys = [
-      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
-    ];
-    substituters = [
-      "https://walker-git.cachix.org"
-    ];
   };
 }
