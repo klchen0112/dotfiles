0b703d2f315df4a5a3eaba1663cb5a803b211e44
klchen0112 <klchen0112@gmail.com>
2025-08-31 03:09:23 +0800

    niri: add keys

--- modules/home/nix/caches.nix
+++ modules/home/nix/caches.nix
@@ -23,6 +23,7 @@
       "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
