113285bcc2665b084b87c961239c927410206785
klchen0112 <klchen0112@gmail.com>
2025-08-31 00:52:11 +0800

    add cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,32 @@
 {
   description = "My Personal NixOS and Darwin System Flake Configuration";
   # Wired using https://nixos-unified.org/autowiring.html
+  nixConfig = {
+    substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=0"
+      "https://mirror.sjtu.edu.cn/nix-channels/store?priority=1"
+      "https://nix-community.cachix.org?priority=2"
+      "https://klchen0112.cachix.org?priority=3"
+      "https://niri.cachix.org?priority=4"
+      "https://cache.nixos.org?priority=5"
+    ];
+    trusted-substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://niri.cachix.org"
+      "https://klchen0112.cachix.org"
+
+    ];
+    trusted-public-keys = [
+      "klchen0112.cachix.org-1:cO5Ek4gcvoWtHslHjWn9U5ymU8ZiN7+tJo0jifbtRz4="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   outputs =
     inputs:
     inputs.nixos-unified.lib.mkFlake {
