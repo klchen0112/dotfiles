5aab5e63ea7ac6588d1d07aff98f56c78b732396
klchen <klchen0112@gmail.com>
2025-09-13 23:47:27 +0800

    init i18n

---
+++ modules/i18n/default.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.homeManager.i18n =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      time.timeZone = "Asia/Shanghai";
+
+      i18n = {
+        defaultLocale = "en_US.UTF-8";
+        extraLocaleSettings = {
+          LC_ADDRESS = "zh_CN.UTF-8";
+          LC_IDENTIFICATION = "zh_CN.UTF-8";
+          LC_MEASUREMENT = "zh_CN.UTF-8";
+          LC_MONETARY = "zh_CN.UTF-8";
+          LC_NAME = "zh_CN.UTF-8";
+          LC_NUMERIC = "zh_CN.UTF-8";
+          LC_PAPER = "zh_CN.UTF-8";
+          LC_TELEPHONE = "zh_CN.UTF-8";
+          LC_TIME = "zh_CN.UTF-8";
+        };
+      };
+    };
+}
