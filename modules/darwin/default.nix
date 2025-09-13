62fb808685191d7ef7efe6ee8aabac0160a994f0
klchen <klchen0112@gmail.com>
2025-09-13 22:40:00 +0800

    add srvos

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -2,18 +2,17 @@
 let
   flake-file.inputs = {
     #  MacOS
-     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    mac-app-util = {
-      url = "github:hraban/mac-app-util";
-    };
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    srvos.url = "github:nix-community/srvos";
   };
   flake.modules.darwin.darwin = {
     imports = [
       inputs.mac-app-util.darwinModules.default
-      # inputs.srvos.darwinModules.desktop
-      # inputs.srvos.darwinModules.mixins-terminfo
-      # inputs.srvos.darwinModules.mixins-nix-experimental
-      # inputs.srvos.darwinModules.mixins-trusted-nix-caches
+      inputs.srvos.darwinModules.desktop
+      inputs.srvos.darwinModules.mixins-terminfo
+      inputs.srvos.darwinModules.mixins-nix-experimental
+      inputs.srvos.darwinModules.mixins-trusted-nix-caches
     ];
   };
   flake.modules.homeManager.desktop =
