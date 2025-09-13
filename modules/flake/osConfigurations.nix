c979a5e97369bd80190aad556cd8bd7d03547556
klchen <klchen0112@gmail.com>
2025-09-14 01:53:55 +0800

    add a99r50

--- modules/flake/osConfigurations.nix
+++ modules/flake/osConfigurations.nix
@@ -1,11 +1,14 @@
 { inputs, ... }:
 let
   inherit (inputs.self.lib.mk-os)
-    darwin
+    darwin linux
     ;
 in
 {
   flake.darwinConfigurations = {
     mbp-m1 = darwin "mbp-m1";
+  };
+  flake.nixosConfigurations = {
+    a99r50 = linux "a99r50";
   };
 }
