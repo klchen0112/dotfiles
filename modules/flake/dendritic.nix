c16b33b983ac03e7b16e11fb73ef2b498e2a7859
klchen <klchen0112@gmail.com>
2025-09-13 22:36:03 +0800

    update lock

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -7,10 +7,5 @@
   flake-file.inputs = {
     flake-file.url = "github:vic/flake-file";
   };
-  flake-file.write-hooks = {
-    enable = true;
-  };
-  flake-file.check-hooks = {
-    enable = true;
-  };
+  
 }
