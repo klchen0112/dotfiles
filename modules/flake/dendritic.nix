448163bf5d59c23c0972ea1a5672f0d55079567a
klchen <klchen0112@gmail.com>
2025-09-13 23:32:05 +0800

    add lock

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -3,9 +3,13 @@
 
   imports = [
     inputs.flake-file.flakeModules.dendritic
+    inputs.flake-file.flakeModules.allfollow
   ];
   flake-file.inputs = {
     flake-file.url = "github:vic/flake-file";
   };
-  
+  flake-file.prune-lock = {
+    enable = true;
+  };
+
 }
