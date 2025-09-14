306b971816b8e412b2f59e917a1dcdb6df390457
klchen <klchen0112@gmail.com>
2025-09-14 17:53:26 +0000

    update flake config

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -3,13 +3,12 @@
 
   imports = [
     inputs.flake-file.flakeModules.dendritic
-    inputs.flake-file.flakeModules.allfollow
   ];
   flake-file.inputs = {
     flake-file.url = "github:vic/flake-file";
+    allfollow.url = "github:spikespaz/allfollow";
   };
   flake-file.prune-lock = {
     enable = true;
   };
-
 }
