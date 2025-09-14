f78d2915d5483122d74306a0d35c173882c72432
klchen <klchen0112@gmail.com>
2025-09-14 17:59:41 +0000

    add nixos config

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -7,8 +7,11 @@
   flake-file.inputs = {
     flake-file.url = "github:vic/flake-file";
     allfollow.url = "github:spikespaz/allfollow";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
   flake-file.prune-lock = {
     enable = true;
   };
+  systems = import inputs.systems;
 }
