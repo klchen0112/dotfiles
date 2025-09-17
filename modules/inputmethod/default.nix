133ab3238af49276c1a28549a6e0a551ea1eb637
klchen <klchen0112@gmail.com>
2025-09-18 01:18:34 +0800

    update rime settings

--- modules/inputmethod/default.nix
+++ modules/inputmethod/default.nix
@@ -1,38 +1,63 @@
+{ inputs, ... }:
 {
+  flake-file.inputs = {
+    rime = {
+      url = "github:klchen0112/rime-snow-combo-pinyin";
+      flake = false;
+    };
+  };
   flake.modules.homeManager.inputmethod =
     {
       pkgs,
-      config,
+      lib,
       ...
     }:
     let
-      rimePath = "${config.home.homeDirectory}/my/dotfiles/rime";
+      my-rime-data = pkgs.stdenv.mkDerivation {
+        name = "my-rime-config-data";
+        src = inputs.rime; # 'self' 指向 flake 自身
+        installPhase = ''
+          mkdir -p $out
+          cp -r ./* $out/
+        '';
+      };
+      rime-install = pkgs.writeShellApplication {
+        name = "doom-install";
+        runtimeInputs = with pkgs; [
+          git
+          openssh
+        ];
+        text = ''
+          if test "''${rime_rev:-}" = "${inputs.rime.rev}"; then
+            echo "Rime already at revision ${inputs.rime.rev}"
+            exit 0
+          fi
+
+          (
+            echo "DOOM Emacs obtaining revision ${inputs.doom-emacs.rev}"
+            if ! test -d "$HOME/Library/Rime/.git"; then
+              git clone --depth 1 https://github.com/klchen0112/rime-snow-combo-pinyin "$HOME/Library/Rime"
+            fi
+            cd "$HOME/Library/Rime"
+            git fetch --depth 1 origin "${inputs.rime.rev}"
+            git reset --hard "${inputs.rime.rev}"
+          )
+        '';
+      };
     in
     {
       i18n.inputMethod = {
         enable = pkgs.stdenv.isLinux;
         type = "fcitx5";
         fcitx5.addons = with pkgs; [
-          fcitx5-rime
+          fcitx5-rime.override
+          { rimeDataPkgs = [ my-rime-data ]; }
           fcitx5-configtool
           fcitx5-gtk # gtk im module
         ];
       };
-      home.file = {
-        ".config/fcitx5/conf/classicui.conf" = {
-          enable = pkgs.stdenv.isLinux;
-          source = ./classicui.conf;
-        };
-        ".local/share/fcitx5/rime" = {
-          enable = pkgs.stdenv.isLinux;
-          source = config.lib.file.mkOutOfStoreSymlink rimePath;
-        };
 
-        "Library/Rime" = {
-          enable = pkgs.stdenv.isDarwin;
-          source = config.lib.file.mkOutOfStoreSymlink rimePath;
-        };
+      home.activation.rime-install = lib.optionalAttrs pkgs.stdenv.isDarwin ''run ${lib.getExe rime-install}'';
 
-      };
     };
 }
