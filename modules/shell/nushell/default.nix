bdbec9bba61240639bcdbe00e9597c70a22a211e
klchen <klchen0112@gmail.com>
2025-09-23 19:25:04 +0800

    fmt files

--- modules/shell/nushell/default.nix
+++ modules/shell/nushell/default.nix
@@ -36,17 +36,17 @@
           show_banner = false;
         };
         envFile.text = ''
-if (sys host).name == "Darwin" {
-    $env.PATH = (
-        $env.PATH
-        | prepend $"/etc/profiles/per-user/($env.USER)/bin"
-        | prepend $"/Users/($env.USER)/.nix-profile/bin"
-        | prepend "/run/wrappers/bin"
-        | prepend "/run/current-system/sw/bin"
-        | prepend "/nix/var/nix/profiles/default/bin"
-        | uniq
-    )
-}
+          if (sys host).name == "Darwin" {
+              $env.PATH = (
+                  $env.PATH
+                  | prepend $"/etc/profiles/per-user/($env.USER)/bin"
+                  | prepend $"/Users/($env.USER)/.nix-profile/bin"
+                  | prepend "/run/wrappers/bin"
+                  | prepend "/run/current-system/sw/bin"
+                  | prepend "/nix/var/nix/profiles/default/bin"
+                  | uniq
+              )
+          }
         '';
         plugins = with pkgs.nushellPlugins; [
           polars
