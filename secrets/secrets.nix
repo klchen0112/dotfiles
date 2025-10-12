e3dab21143f406362bb115edbef9f35101f49834
klchen <klchen0112@gmail.com>
2025-10-13 00:04:17 +0800

    add secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,27 @@
 let
-  config = import ../config.nix;
-
-  allUsersSshKeys = builtins.concatLists (map (user: user.sshKey) (builtins.attrValues config.users));
-  allMachinesSshKeys = builtins.concatLists (
-    map (machine: machine.sshKey) (builtins.attrValues config.machines)
-  );
+  klchen = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGszCNQqxT1/s6sYjj1aewvCjaa3D7UwoOM7UD5K+ha klchen0112@mbp-m1"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx1SNaQZ6v1onDSGz1wNX1W3zIf2KkTERjKGC+k157D klchen@sanjiao"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/c10VIo81cztYJza3e+l1JlwsTJQk1lhBOypGhYn3T klchen@a3400g"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPI6HctaCnuhyOdbrYs2un7/QA/hqFPfDVRlL0klfhGc klchen@i12r20"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNgI2fAHSDQCB+DgZPsjGF+arPudVmWS4hTXbJCvwwX klchen@a99r50"
+  ];
+  users = klchen;
+  a99r50 = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRBuSM5DLKYUtS1gmoZEA+y2xGrWWtxs3HEutD1LCwx"
+  ];
+  sanjiao = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8T9wyRN9CA/wWN70aHaRoAi1BRFeWkIjfL6+sycRaI"
+  ];
+  i12r20 = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3hO4yhyrO8JHbP6yokAEbRDPb4FR/bhtoIb2rIBP5q root@i12r20"
+  ];
+  a3400g = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsw5gk6koAb2D1SYnHt3jhYLNCWChR6eFKJ3vPO3tZY"
+  ];
+  machines = a99r50 ++ sanjiao ++ i12r20 ++ a3400g;
 in
 {
-  "access-tokens.age".publicKeys = (allUsersSshKeys ++ allMachinesSshKeys);
+  "access-tokens.age".publicKeys = users ++ machines;
+  "gpg.age".publicKeys = users;
 }
