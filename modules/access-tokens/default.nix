e276cdb13c7c75603bf68b39cec1e92a749f84da
klchen <klchen0112@gmail.com>
2026-01-04 17:41:58 +0000

    fix(age): add identiyPaths

--- modules/access-tokens/default.nix
+++ modules/access-tokens/default.nix
@@ -25,6 +25,7 @@
       nix.extraOptions = ''
         !include ${config.age.secrets.access-tokens.path}
       '';
+      age.identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
       age.secretsDir = "${config.home.homeDirectory}/.local/share/agenix/agenix";
       age.secretsMountPoint = "${config.home.homeDirectory}/.local/share/agenix/agenix.d";
     };
@@ -40,6 +41,7 @@
         inputs.agenix.packages.${pkgs.system}.default
       ];
       imports = [ inputs.agenix.nixosModules.default ];
+      age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       age.secrets.access-tokens.file = inputs.self + /secrets/access-tokens.age;
       nix.extraOptions = ''
         !include ${config.age.secrets.access-tokens.path}
@@ -56,6 +58,7 @@
         inputs.agenix.packages.${pkgs.system}.default
       ];
       imports = [ inputs.agenix.darwinModules.default ];
+      age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       age.secrets.access-tokens.file = inputs.self + /secrets/access-tokens.age;
       nix.extraOptions = ''
         !include ${config.age.secrets.access-tokens.path}
