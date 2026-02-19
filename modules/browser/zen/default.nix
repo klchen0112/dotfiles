e010ec6146279e5025213f1baec10ce5a8aff1af
klchen <klchen0112@gmail.com>
2026-02-19 20:01:48 +0800

    fix(zen): update 1.18b

--- modules/browser/zen/default.nix
+++ modules/browser/zen/default.nix
@@ -31,6 +31,43 @@
         # inputs.nix-darwin-browsers.overlays
       ];
       stylix.targets.zen-browser.profileNames = [ "${config.home.username}" ];
+      xdg.mimeApps =
+        let
+          value =
+            let
+              zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform}.beta; # or twilight
+            in
+            zen-browser.meta.desktopFileName;
+
+          associations = builtins.listToAttrs (
+            map
+              (name: {
+                inherit name value;
+              })
+              [
+                "application/x-extension-shtml"
+                "application/x-extension-xhtml"
+                "application/x-extension-html"
+                "application/x-extension-xht"
+                "application/x-extension-htm"
+                "x-scheme-handler/unknown"
+                "x-scheme-handler/mailto"
+                "x-scheme-handler/chrome"
+                "x-scheme-handler/about"
+                "x-scheme-handler/https"
+                "x-scheme-handler/http"
+                "application/xhtml+xml"
+                "application/json"
+                "text/plain"
+                "text/html"
+              ]
+          );
+        in
+        {
+          associations.added = associations;
+          defaultApplications = associations;
+        };
+
       programs.zen-browser = {
         enable = true;
         enableGnomeExtensions = false;
