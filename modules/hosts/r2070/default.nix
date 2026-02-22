488186e2be9cc6f33d96a59599cc4edd9106a6ff
klchen <klchen0112@gmail.com>
2026-02-22 21:57:02 +0800

    fix(r2070): fix ssh

--- modules/hosts/r2070/default.nix
+++ modules/hosts/r2070/default.nix
@@ -59,5 +59,14 @@ in
       home-manager.users.klchen.imports = with config.flake.modules.homeManager; [
         bash
       ];
+      environment.etc = {
+        "machine-id".source = "/nix/persist/etc/machine-id";
+        "ssh/ssh_host_rsa_key".source = "/nix/persist/etc/ssh/ssh_host_rsa_key";
+        "ssh/ssh_host_rsa_key.pub".source = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
+        "ssh/ssh_host_ed25519_key".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
+        "ssh/ssh_host_ed25519_key.pub".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
+      };
+
     };
+
 }
