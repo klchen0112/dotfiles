832512094013186ba75ecab7b3a6d498e73f537e
klchen0112 <klchen0112@gmail.com>
2026-08-10 18:52:36 +0800

    feat(nix): add mbp-m1 remote-builder access

--- modules/nix/build-machines.nix
+++ modules/nix/build-machines.nix
@@ -13,6 +13,7 @@
             [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3hO4yhyrO8JHbP6yokAEbRDPb4FR/bhtoIb2rIBP5q root@i12r20"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRBuSM5DLKYUtS1gmoZEA+y2xGrWWtxs3HEutD1LCwx root@init"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9ZvdIrZP9su70iBKgCB0QOY0kL9Z9qu3B9Of05VS5a root@mbp-m1"
             ];
         };
         nix.settings.trusted-users = [ "remotebuild" ];
